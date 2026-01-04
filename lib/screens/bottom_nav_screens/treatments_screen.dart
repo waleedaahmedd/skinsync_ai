0fba15f77b03a91482e65ac8f4b2be86c076e075
WALEED AHMED <waleed.ahmed@appedology.com>
2026-01-04 07:38:57 +0500

    feat: Implement scrollable treatment screen and restore widget
    - Wraps the main column in `treatments_screen.dart` with a `SingleChildScrollView` to fix overflow issues.
    - Adds `physics: NeverScrollableScrollPhysics()` and `shrinkWrap: true` to the `GridView.builder` in `treament_list_screen.dart` to allow it to scroll within the `SingleChildScrollView`.
    - Restores the `RecommendedTreatmentContainer` widget by uncommenting its code.
    - Re-enables the filter list and treatment sections in `treatments_screen.dart` by uncommenting the relevant code blocks.
    - Updates the search hint text to be more descriptive.

--- lib/screens/bottom_nav_screens/treatments_screen.dart
+++ lib/screens/bottom_nav_screens/treatments_screen.dart
@@ -37,37 +37,39 @@ class TreatmentsScreen extends StatelessWidget {
           onTap: () {},
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 15.h),
-                TextField(
-                  style: CustomFonts.black18w400,
 
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    hintText: "Search treatment",
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 15.h),
+                  TextField(
+                    style: CustomFonts.black18w400,
+
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search  doctor, injector, treatment & clinic",
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-         // SizedBox(height: 15.h),
-          Expanded(
-            child: Consumer(
+            SizedBox(height: 15.h),
+            Consumer(
               builder: (context, ref, _) {
                 return ref.watch(treatmentViewModel)
                     ? TreatmentMainScreen()
                     : TreamentListScreen();
               },
             ),
-          ),
-        ],
+            SizedBox(height: 170.h),
+          ],
+        ),
       ),
     );
   }
@@ -81,12 +83,12 @@ class TreatmentMainScreen extends StatefulWidget {
 }
 
 int selectedFilterIndex = 0;
-// List<Fillter> fillter = [
-//   Fillter(title: "All Treatment"),
-//   Fillter(title: "Injectables & Fillers ", svg: SvgAssets.treatment),
-//   Fillter(title: "Laser Treatments", svg: SvgAssets.treatment),
-//   Fillter(title: "Sculpting & Contouring", svg: SvgAssets.treatment),
-// ];
+List<Fillter> fillter = [
+  Fillter(title: "All Treatment"),
+  Fillter(title: "Injectables & Fillers ", svg: SvgAssets.treatment),
+  Fillter(title: "Laser Treatments", svg: SvgAssets.treatment),
+  Fillter(title: "Sculpting & Contouring", svg: SvgAssets.treatment),
+];
 
 class _TreatmentMainScreenState extends State<TreatmentMainScreen> {
   @override
@@ -96,186 +98,188 @@ class _TreatmentMainScreenState extends State<TreatmentMainScreen> {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          //  SizedBox(
-            //  height: 0.h,
-              // child: ListView.builder(
-              //   scrollDirection: Axis.horizontal,
-              //   itemCount: fillter.length,
-              //   itemBuilder: (context, index) {
-              //     return Padding(
-              //       padding: EdgeInsets.only(
-              //         left: index == 0 ? 30.w : 0,
-              //         right: 10.w,
-              //       ),
-              //       child: FillterContainer(
-              //         isSelected: selectedFilterIndex == index,
-              //         title: fillter[index].title,
-              //         svgImage: fillter[index].svg,
-              //         onTap: () {
-              //           setState(() {
-              //             selectedFilterIndex = index;
-              //           });
-              //         },
-              //       ),
-              //     );
-              //   },
-              // ),
-           // ),
+            SizedBox(
+              height: 50.h,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: fillter.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? 30.w : 0,
+                      right: 10.w,
+                    ),
+                    child: FillterContainer(
+                      isSelected: selectedFilterIndex == index,
+                      title: fillter[index].title,
+                      svgImage: fillter[index].svg,
+                      onTap: () {
+                        setState(() {
+                          selectedFilterIndex = index;
+                        });
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
             SizedBox(height: 32.h),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 30.0.w),
               child: Text(
-                "Select Treatment",
+                "Recommended Treatments",
                 style: CustomFonts.black24w600,
               ),
             ),
-            // SizedBox(
-            //   height: 352.h,
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     //itemCount: fillter.length,
-            //     itemBuilder: (context, index) {
-            //       return Padding(
-            //         padding: EdgeInsets.only(
-            //           left: index == 0 ? 30.w : 0,
-            //           right: 20.w,
-            //           top: 28.h,
-            //           bottom: 25.h,
-            //         ),
-            //         child: RecommendedTreatmentContainer(
-            //           treatmentImage: PngAssets.laserTreatment,
-            //           treatmentName: "Laser Treatment",
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 25.h),
-            // Padding(
-            //   padding: EdgeInsets.symmetric(horizontal: 30.w),
-            //   child: HeadingWithRightArrow(
-            //     title: "Skincare & Facial Treatments",
-            //     onTap: () {
-            //       ref
-            //           .read(treatmentViewModel.notifier)
-            //           .setTreatmentMainScreen(value: false);
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 13.h),
-            // SizedBox(
-            //   height: 221.h,
-            //
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemCount: 4,
-            //     itemBuilder: (context, index) {
-            //       return Padding(
-            //         padding: EdgeInsets.only(
-            //           left: index == 0 ? 30.w : 0,
-            //           right: 12.w,
-            //         ),
-            //         child: TreatmentContainer(
-            //           treatmentName: "Botox Treatment",
-            //           clinicName: "Glow Skin Clinic",
-            //           dateTime: "October 20, 3:00 PM",
-            //           treatmentimage: DummyAssets.treatmentimage,
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 25.h),
-            // Padding(
-            //   padding: EdgeInsets.symmetric(horizontal: 30.w),
-            //   child: HeadingWithRightArrow(
-            //     title: "Injectables & Fillers ",
-            //     onTap: () {
-            //       ref
-            //           .read(treatmentViewModel.notifier)
-            //           .setTreatmentMainScreen(value: false);
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 13.h),
-            // SizedBox(
-            //   height: 221.h,
-            //
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemCount: 4,
-            //     itemBuilder: (context, index) {
-            //       return Padding(
-            //         padding: EdgeInsets.only(
-            //           left: index == 0 ? 30.w : 0,
-            //           right: 12.w,
-            //         ),
-            //         child: TreatmentContainer(
-            //           treatmentName: "Botox Treatment",
-            //           clinicName: "Glow Skin Clinic",
-            //           dateTime: "October 20, 3:00 PM",
-            //           treatmentimage: DummyAssets.treatmentimage,
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 25.h),
-            // Padding(
-            //   padding: EdgeInsets.symmetric(horizontal: 30.w),
-            //   child: HeadingWithRightArrow(
-            //     title: "Laser Treatments",
-            //     onTap: () {
-            //       ref
-            //           .read(treatmentViewModel.notifier)
-            //           .setTreatmentMainScreen(value: false);
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 13.h),
-            // SizedBox(
-            //   height: 221.h,
-            //
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemCount: 4,
-            //     itemBuilder: (context, index) {
-            //       return Padding(
-            //         padding: EdgeInsets.only(
-            //           left: index == 0 ? 30.w : 0,
-            //           right: 12.w,
-            //         ),
-            //         child: TreatmentContainer(
-            //           treatmentName: "Botox Treatment",
-            //           clinicName: "Glow Skin Clinic",
-            //           dateTime: "October 20, 3:00 PM",
-            //           treatmentimage: DummyAssets.treatmentimage,
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(height: 25.h),
-            // Padding(
-            //   padding: EdgeInsets.symmetric(horizontal: 30.w),
-            //   child: HeadingWithRightArrow(
-            //     title: "Sculpting & Contouring",
-            //     onTap: () {
-            //       ref
-            //           .read(treatmentViewModel.notifier)
-            //           .setTreatmentMainScreen(value: false);
-            //     },
-            //   ),
-            // ),
-            SizedBox(height: 25.h),
-            Expanded(
+            SizedBox(
+              height: 352.h,
               child: ListView.builder(
-                scrollDirection: Axis.vertical,
+                scrollDirection: Axis.horizontal,
+                itemCount: fillter.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? 30.w : 0,
+                      right: 20.w,
+                      top: 28.h,
+                      bottom: 25.h,
+                    ),
+                    child: RecommendedTreatmentContainer(
+                      treatmentImage: PngAssets.laserTreatment,
+                      treatmentName: "Laser Treatment",
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 25.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: HeadingWithRightArrow(
+                title: "Skincare & Facial Treatments",
+                onTap: () {
+                  ref
+                      .read(treatmentViewModel.notifier)
+                      .setTreatmentMainScreen(value: false);
+                },
+              ),
+            ),
+            SizedBox(height: 13.h),
+            SizedBox(
+              height: 221.h,
+
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
                 itemCount: 4,
                 itemBuilder: (context, index) {
                   return Padding(
                     padding: EdgeInsets.only(
-                      left: 12.w,
+                      left: index == 0 ? 30.w : 0,
+                      right: 12.w,
+                    ),
+                    child: TreatmentContainer(
+                      treatmentName: "Botox Treatment",
+                      clinicName: "Glow Skin Clinic",
+                      dateTime: "October 20, 3:00 PM",
+                      treatmentimage: DummyAssets.treatmentimage,
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 25.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: HeadingWithRightArrow(
+                title: "Injectables & Fillers ",
+                onTap: () {
+                  ref
+                      .read(treatmentViewModel.notifier)
+                      .setTreatmentMainScreen(value: false);
+                },
+              ),
+            ),
+            SizedBox(height: 13.h),
+            SizedBox(
+              height: 221.h,
+
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? 30.w : 0,
+                      right: 12.w,
+                    ),
+                    child: TreatmentContainer(
+                      treatmentName: "Botox Treatment",
+                      clinicName: "Glow Skin Clinic",
+                      dateTime: "October 20, 3:00 PM",
+                      treatmentimage: DummyAssets.treatmentimage,
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 25.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: HeadingWithRightArrow(
+                title: "Laser Treatments",
+                onTap: () {
+                  ref
+                      .read(treatmentViewModel.notifier)
+                      .setTreatmentMainScreen(value: false);
+                },
+              ),
+            ),
+            SizedBox(height: 13.h),
+            SizedBox(
+              height: 221.h,
+
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? 30.w : 0,
+                      right: 12.w,
+                    ),
+                    child: TreatmentContainer(
+                      treatmentName: "Botox Treatment",
+                      clinicName: "Glow Skin Clinic",
+                      dateTime: "October 20, 3:00 PM",
+                      treatmentimage: DummyAssets.treatmentimage,
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 25.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: HeadingWithRightArrow(
+                title: "Sculpting & Contouring",
+                onTap: () {
+                  ref
+                      .read(treatmentViewModel.notifier)
+                      .setTreatmentMainScreen(value: false);
+                },
+              ),
+            ),
+            SizedBox(height: 13.h),
+            SizedBox(
+              height: 221.h,
+
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? 30.w : 0,
                       right: 12.w,
                     ),
                     child: TreatmentContainer(
--- lib/widgets/recommended_treatment_container.dart
+++ lib/widgets/recommended_treatment_container.dart
@@ -1,47 +1,47 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:skinsync_ai/screens/treatment_detail_screen.dart';
-// import 'package:skinsync_ai/utills/assets.dart';
-// import 'package:skinsync_ai/utills/custom_fonts.dart';
-//
-// class RecommendedTreatmentContainer extends StatelessWidget {
-//   final String treatmentImage;
-//   final String treatmentName;
-//   const RecommendedTreatmentContainer({
-//     super.key,
-//     required this.treatmentImage,
-//     required this.treatmentName,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap:(){
-//         Navigator.pushNamed(context,TreatmentDetailScreen.routeName);
-//       },
-//       child: Container(
-//         height: 299.h,
-//         width: 279.w,
-//         padding: EdgeInsets.only(bottom: 20.w),
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//             image: AssetImage(treatmentImage),
-//             fit: BoxFit.fill,
-//           ),
-//           borderRadius: BorderRadius.circular(10.r),
-//         ),
-//         child: Align(
-//           alignment: Alignment.bottomCenter,
-//           child: Container(
-//             padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 8.h),
-//             decoration: BoxDecoration(
-//               borderRadius: BorderRadius.circular(50.r),
-//               color: Colors.transparent.withValues(alpha: 0.2),
-//             ),
-//             child: Text(treatmentName, style: CustomFonts.white14w600),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skinsync_ai/screens/treatment_detail_screen.dart';
+import 'package:skinsync_ai/utills/assets.dart';
+import 'package:skinsync_ai/utills/custom_fonts.dart';
+
+class RecommendedTreatmentContainer extends StatelessWidget {
+  final String treatmentImage;
+  final String treatmentName;
+  const RecommendedTreatmentContainer({
+    super.key,
+    required this.treatmentImage,
+    required this.treatmentName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap:(){
+        Navigator.pushNamed(context,TreatmentDetailScreen.routeName);
+      },
+      child: Container(
+        height: 299.h,
+        width: 279.w,
+        padding: EdgeInsets.only(bottom: 20.w),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(treatmentImage),
+            fit: BoxFit.fill,
+          ),
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 8.h),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50.r),
+              color: Colors.transparent.withValues(alpha: 0.2),
+            ),
+            child: Text(treatmentName, style: CustomFonts.white14w600),
+          ),
+        ),
+      ),
+    );
+  }
+}
