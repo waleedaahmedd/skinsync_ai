f2b8c533082690d2296c4eb4c8f76ec6dc1a57c7
tauqeerkhattak <tauqeer745@outlook.com>
2026-03-09 20:59:25 +0500

    feat: Integrate Firebase into the Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_init.dart';
+import 'firebase_options.dart';
 import 'services/storage_service.dart';
 import 'utills/secure_storage_service.dart';
 import 'utills/shared_pref.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ScreenUtil.ensureScreenSize();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await SharedPref.init();
