bb56c3373b781bb6a346cb7f0faf3232d3b434e7
WALEED AHMED <waleed.allshore@gmail.com>
2026-08-10 20:12:50 +0500

    feat: update Gradle and dependencies for improved performance and compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,22 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.withId("com.android.application") {
+        dependencies {
+            add("implementation", "androidx.concurrent:concurrent-futures:1.2.0")
+        }
+    }
+    plugins.withId("com.android.library") {
+        dependencies {
+            add("implementation", "androidx.concurrent:concurrent-futures:1.2.0")
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
