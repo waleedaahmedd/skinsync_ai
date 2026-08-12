0629a1a05a0f6925caad251331c532f7b05884e3
WALEED AHMED <waleed.allshore@gmail.com>
2026-08-12 17:30:35 +0500

    feat: update API base URL for QA environment and enable deployment mode

--- lib/utills/enums.dart
+++ lib/utills/enums.dart
@@ -65,7 +65,7 @@ enum EndPoints {
 }
 
 enum BaseUrls {
-  api("https://api.skinsyncai.com/api/"),
+  api('https://api-qa.skinsyncai.com/api/'),
   apiQa('https://api-qa.skinsyncai.com/api/');
 
   final String url;
