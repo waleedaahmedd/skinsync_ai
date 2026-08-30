95db008885733e3c4143e9fcc207425536bfab23
WALEED AHMED <waleed.allshore@gmail.com>
2026-08-30 21:30:56 +0500

    feat: enable navigation to personal details from app bar profile section

--- lib/widgets/app_bar_with_action_icon.dart
+++ lib/widgets/app_bar_with_action_icon.dart
@@ -6,6 +6,7 @@ import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
 import '../utils/custom_fonts.dart';
 import '../view_models/auth_view_model.dart';
 
+import '../screens/personal_detail_screen.dart';
 import '../utils/color_constant.dart';
 
 class AppBarWithActionIcon extends StatelessWidget
@@ -36,63 +37,72 @@ class AppBarWithActionIcon extends StatelessWidget
                       final userDetails = state.authData?.user;
                       final name = userDetails?.name;
 
-                      return Row(
-                        children: [
-                          // Optional Profile Image Avatar
-                          Container(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: CustomColors.purpleColor.withValues(
-                                  alpha: 0.5,
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            PersonalDetailScreen.routeName,
+                          );
+                        },
+                        child: Row(
+                          children: [
+                            // Optional Profile Image Avatar
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: CustomColors.purpleColor.withValues(
+                                    alpha: 0.5,
+                                  ),
+                                  width: context.w(2),
                                 ),
-                                width: context.w(2),
                               ),
-                            ),
-                            child: ClipOval(
-                              child: CachedNetworkImage(
-                                imageUrl: userDetails?.profileImageUrl ?? "",
-                                height: context.w(48),
-                                width: context.w(48),
-                                fit: BoxFit.cover,
-                                placeholder: (context, url) =>
-                                    const CupertinoActivityIndicator(),
-                                errorWidget: (context, url, error) => Container(
+                              child: ClipOval(
+                                child: CachedNetworkImage(
+                                  imageUrl: userDetails?.profileImageUrl ?? "",
                                   height: context.w(48),
                                   width: context.w(48),
-                                  color: Colors.grey.shade100,
-                                  child: Icon(
-                                    Icons.person_outline_rounded,
-                                    size: context.sp(24),
-                                    color: Colors.grey,
-                                  ),
+                                  fit: BoxFit.cover,
+                                  placeholder: (context, url) =>
+                                      const CupertinoActivityIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      Container(
+                                        height: context.w(48),
+                                        width: context.w(48),
+                                        color: Colors.grey.shade100,
+                                        child: Icon(
+                                          Icons.person_outline_rounded,
+                                          size: context.sp(24),
+                                          color: Colors.grey,
+                                        ),
+                                      ),
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox(width: context.w(12)),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Hello, ${name ?? "Guest"}!',
-                                  style: CustomFonts.black22w600,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                ),
-                                SizedBox(height: context.h(2)),
-                                Text(
-                                  'Your aesthetic journey starts here.',
-                                  style: CustomFonts.grey12w400,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                ),
-                              ],
+                            SizedBox(width: context.w(12)),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Hello, ${name ?? "Guest"}!',
+                                    style: CustomFonts.black22w600,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                  SizedBox(height: context.h(2)),
+                                  Text(
+                                    'Your aesthetic journey starts here.',
+                                    style: CustomFonts.grey12w400,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       );
                     },
                   ),
