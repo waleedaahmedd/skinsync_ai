fed775b234154f730d557df90157fa715bdb7088
saboor289 <syedsuboorhaiderjillani@gmail.com>
2026-08-31 06:43:42 +0500

    feat: update preferred slots bottom sheet to include selection validation and dynamic button text

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -84,8 +84,8 @@ enum Status { active, inactive }
 
 enum BaseUrls {
   api('https://api.skinsyncai.com/api/'),
- // apiQa('https://api-dev.skinsyncai.com/api/');
- apiQa('https://q4nw0p7g-8084.asse.devtunnels.ms/api/');
+ apiQa('https://api-dev.skinsyncai.com/api/');
+ //apiQa('https://q4nw0p7g-8084.asse.devtunnels.ms/api/');
 
   final String url;
   const BaseUrls(this.url);
